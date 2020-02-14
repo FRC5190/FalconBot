a8b9d1419946a26e48af52348d549d222b43d221
Goobwabber <47616186+Goobwabber@users.noreply.github.com>
2020-02-14 10:42:56 -0500

    Close discord client before shutting down

--- src/main/kotlin/services/jda/commands/ownerlevel/EndCommand.kt
+++ src/main/kotlin/services/jda/commands/ownerlevel/EndCommand.kt
@@ -1,6 +1,8 @@
 package services.jda.commands.ownerlevel
 
+import net.dv8tion.jda.api.JDA
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent
+import services.JDAService
 import services.jda.commands.Command
 import services.jda.commands.CommandPermssionLevel
 
@@ -14,6 +16,7 @@ object EndCommand : Command(
 ) {
     override fun execute(event: MessageReceivedEvent, args: List<String>) {
         if (event.author.id == "277200664424218634") {
+            JDAService.service.shutdownNow()
             System.exit(0)
         }
     }
