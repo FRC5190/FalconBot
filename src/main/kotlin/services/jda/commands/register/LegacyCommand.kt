40a17646af031e8eb98ea9db1e5e57ccd58c5346
Goobwabber <47616186+Goobwabber@users.noreply.github.com>
2020-02-14 10:51:43 -0500

    fix for legacy command

--- src/main/kotlin/services/jda/commands/register/LegacyCommand.kt
+++ src/main/kotlin/services/jda/commands/register/LegacyCommand.kt
@@ -20,28 +20,27 @@ object LegacyCommand : Command(
                 .get(SheetsConstants.falconusersSheet, "Sheet1!A2:B1000")
                 .execute()
 
-            var values = data.getValues()
-            var targetRow =  if (values.any { it[0] == args[2] }) {
-                values.indexOf(values.find { it[0] == args[2] }!!)
+            val values = data.getValues()
+
+            if (values.any { it[1] == event.author.id}) {
+                event.channel.sendMessage("You are already registered!").queue()
             } else {
-                values.count() + 1
-            }
-
-            val userData = GoogleSheets.service.spreadsheets().values()
-                .get(SheetsConstants.falcontimeSheet, "Current!A2:J1000")
-                .execute()
-
-            val userValues = userData.getValues()
-            if (userValues.any { it[0] == args[2] }) {
-                values.add(targetRow, listOf(args[2], event.author.id))
-                data.setValues(values)
-                GoogleSheets.service.spreadsheets().values()
-                    .update(SheetsConstants.falconusersSheet, "Sheet1!A2:B1000", data)
+                val userValues = GoogleSheets.service.spreadsheets().values()
+                    .get(SheetsConstants.falcontimeSheet, "Current!A2:J1000")
                     .execute()
+                    .getValues()
 
-                event.channel.sendMessage("FalconTime account and Discord account linked!")
-            } else {
-                event.channel.sendMessage("A FalconTime account was not found under that id.")
+                if (userValues.any { it[0] == args[2] }) {
+                    values.add(listOf(args[2], event.author.id))
+                    data.setValues(values)
+                    GoogleSheets.service.spreadsheets().values()
+                        .update(SheetsConstants.falconusersSheet, "Sheet1!A2:B1000", data)
+                        .execute()
+
+                    event.channel.sendMessage("FalconTime and Discord account linked!")
+                } else {
+                    event.channel.sendMessage("A FalconTime account was not found under that id.")
+                }
             }
         }
     }
