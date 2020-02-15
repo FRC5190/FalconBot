cfe014a820a677abdd5d67b11ef686ce0788059a
Goobwabber <47616186+Goobwabber@users.noreply.github.com>
2020-02-14 20:33:13 -0500

    try again (11)

--- src/main/kotlin/services/jda/commands/attendance/AttendanceCommand.kt
+++ src/main/kotlin/services/jda/commands/attendance/AttendanceCommand.kt
@@ -75,8 +75,7 @@ object AttendanceCommand : Command(
                     val totalTime = Duration.ofHours(timeParts[0].toLong())
                         .plusMinutes(timeParts[1].toLong())
                         .plusSeconds(timeParts[2].toLong())
-
-                    totalTime.plusSeconds(lastLogin.until(currentTime, ChronoUnit.SECONDS))
+                        .plusSeconds(lastLogin.until(currentTime, ChronoUnit.SECONDS))
 
                     row[9] = String.format("%d:%02d:%02d", totalTime.seconds / 3600, (totalTime.seconds % 3600) / 60, (totalTime.seconds % 60));
                 }
