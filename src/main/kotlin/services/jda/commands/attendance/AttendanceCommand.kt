06d97ce5f6512ae9a180a9cbafeff929e25abeb0
Goobwabber <47616186+Goobwabber@users.noreply.github.com>
2020-02-18 16:45:42 -0500

    lol change that number back

--- src/main/kotlin/services/jda/commands/attendance/AttendanceCommand.kt
+++ src/main/kotlin/services/jda/commands/attendance/AttendanceCommand.kt
@@ -28,7 +28,7 @@ object AttendanceCommand : Command(
             .sortWeeklyHours()
 
         val member = members.find { member ->
-            member.discordID == if (args.count() < 1) {
+            member.discordID == if (args.count() < 2) {
                 event.author.id
             } else {
                 event.message.mentionedUsers[0]
