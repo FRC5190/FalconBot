f037a67043acbd4e165c35ce8da862c48b20e31d
Goobwabber <47616186+Goobwabber@users.noreply.github.com>
2020-02-22 09:46:39 -0500

    change date format

--- src/main/kotlin/services/jda/commands/countdown/CountdownCommand.kt
+++ src/main/kotlin/services/jda/commands/countdown/CountdownCommand.kt
@@ -41,7 +41,7 @@ object CountdownCommand: Command(
 
                 val countdown = "${days}d, ${hours}h, ${minutes}m, ${seconds}s"
                 val event = name.split(' ').fold("", {acc, r -> acc + r.capitalize() + " "}).removeSuffix(" ")
-                val date = startTime.format(DateTimeFormatter.ofPattern("M/d/uu"))
+                val date = startTime.format(DateTimeFormatter.ofPattern("M/d/uu hh a"))
 
                 embed.addField("**$event:** $date", countdown, true)
             }
