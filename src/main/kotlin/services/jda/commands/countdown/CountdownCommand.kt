97a277a6759e374d8165d6d5514f01423993d5cb
Goobwabber <47616186+Goobwabber@users.noreply.github.com>
2020-02-21 20:54:09 -0500

    fix countdown parsing

--- src/main/kotlin/services/jda/commands/countdown/CountdownCommand.kt
+++ src/main/kotlin/services/jda/commands/countdown/CountdownCommand.kt
@@ -23,26 +23,25 @@ object CountdownCommand: Command(
             .setTitle("Countdowns")
             .setColor(ColorConstants.FALCON_MAROON)
 
-        Configuration.countdowns.forEach { name, datetime ->
+        Configuration.countdowns.forEach { (name, datetime) ->
             var startTime = LocalDateTime.parse(datetime)
             var currentTime = LocalDateTime.now()
-
-            embed.addField(startTime.toString(), "", true)
 
             if (currentTime.isAfter(startTime)) {
                 Configuration.countdowns = Configuration.countdowns.toMutableMap().apply { remove(name) }
             } else {
-                var days = startTime.until(currentTime, ChronoUnit.DAYS)
-                startTime = startTime.plusDays(days)
-                var hours = startTime.until(currentTime, ChronoUnit.HOURS)
-                startTime = startTime.plusHours(hours)
-                var minutes = startTime.until(currentTime, ChronoUnit.MINUTES)
-                startTime = startTime.plusMinutes(minutes)
-                var seconds = startTime.until(currentTime, ChronoUnit.SECONDS)
+                var days = currentTime.until(startTime, ChronoUnit.DAYS)
+                currentTime = currentTime.plusDays(days)
+                var hours = currentTime.until(startTime, ChronoUnit.HOURS)
+                currentTime = currentTime.plusHours(hours)
+                var minutes = currentTime.until(startTime, ChronoUnit.MINUTES)
+                currentTime = currentTime.plusMinutes(minutes)
+                var seconds = currentTime.until(startTime, ChronoUnit.SECONDS)
+                currentTime = currentTime.plusSeconds(seconds)
 
                 val countdown = "${days}d, ${hours}h, ${minutes}m, ${seconds}s"
                 val event = name.split(' ').fold("", {acc, r -> acc + r.capitalize() + " "})
-                val date = datetime.format(DateTimeFormatter.ofPattern("M/d/Y ha"))
+                val date = datetime.format(DateTimeFormatter.ofPattern("M/d/YY hh aa"))
 
                 embed.addField("**$event:** $date", countdown, true)
             }
